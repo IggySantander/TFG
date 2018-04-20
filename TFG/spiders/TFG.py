a0ea9da919569ab0180ecc5ad27a3b2d04ea7360
Ignacio_Oceja <ignacioo@softcom.com>
2018-04-20 11:28:31 +0200

    reowrked project
    now runs from shell:
    
    scrapy crawl TFG

--- TFG/spiders/TFG.py
+++ TFG/spiders/TFG.py
@@ -1,41 +1,31 @@
 # -*- coding: utf-8 -*-
-from twisted.internet import reactor
 import scrapy
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
-from scrapy.linkextractors import LinkExtractor
+
 import base64
 from scrapy_splash import SplashRequest
 
 
-class TFG(scrapy.Spider):
-    name = 'extract'
+class HomeSpider(scrapy.Spider):
+    name = 'TFG'
+    allowed_domains = ['us.dev.cloud.im']
+    start_urls = 'https://us.dev.cloud.im'
 
     def start_requests(self):
-        url = 'https://stackoverflow.com/'
-        splash_args = {
-            'html': 1,
-            'png': 1
-        }
-        yield SplashRequest(url, self.parse_result, endpoint='render.json', args=splash_args)
+        yield SplashRequest(
+            url=HomeSpider.start_urls,
+            callback=self.parse,
+            endpoint='render.json',
+            args={'wait': 2, 'html': 1, 'png': 1, 'render_all': 1}
+        )
 
-    def parse_result(self, response):
-        hxs = scrapy.Selector(response)
-        for sel in hxs.xpath("//div[@id='job_listings']/a"):
-            imgdata = base64.b64decode(response.data['png'])
-            filename = 'some_image.png'
-            with open(filename, 'wb') as f:
-                f.write(imgdata)
+    def parse(self, response):
+        title = response.css('title').extract_first()
 
+        # full decoded JSON data is available as response.data:
+        png_bytes = base64.b64decode(response.data['png'])
 
+        print title
+        print response.data
 
-
-
-#---------------------------
-
-configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-runner = CrawlerRunner()
-
-d = runner.crawl(TFG)
-d.addBoth(lambda _: reactor.stop())
-reactor.run() # the script will block here until the crawling is finished
+        with open('somefile.png', 'wb') as the_file:
+            the_file.write(png_bytes)
