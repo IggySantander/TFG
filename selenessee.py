8892a971b26843d4a831cfa58df717df0d8c50d5
Ignacio Oceja <33689933+IggySantander@users.noreply.github.com>
2019-04-28 17:48:51 +0200

    added selectors in language pages

--- selenessee.py
+++ selenessee.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.ingrammicrocloud.com/')
 print(driver.title)
-"""driver.save_screenshot("Pagina-principal.png")"""
+driver.save_screenshot("Pagina-principal.png")
 menu=driver.find_element_by_css_selector("#block-mainnavigation > div > ul > li:nth-of-type(4)")
 menu.click()
 time.sleep(3)
@@ -35,4 +35,19 @@ time.sleep(2)
 boton=driver.find_element_by_css_selector('[type*="submit"]')
 boton.click()
 time.sleep(2)
+
+driver.get('https://www.ingrammicrocloud.com/')
+time.sleep(2)
+
+lang=driver.find_element_by_css_selector('.selected-country')
+lang.click()
+time.sleep(2)
+langs=driver.find_elements_by_css_selector('a[href*="www.ingrammicrocloud"]')
+for lang in langs:
+    print lang.text
+    print lang.get_attribute('href')
+lang=driver.find_element_by_css_selector('a[href*=".com/es"]')
+lang.click()
+time.sleep(3)
+
 driver.quit()
