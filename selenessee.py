e61f9582fffbdc536b12c05659fb161aee19a602
Ignacio Oceja <33689933+IggySantander@users.noreply.github.com>
2019-05-02 08:23:35 +0200

    added functins

--- selenessee.py
+++ selenessee.py
@@ -3,6 +3,7 @@ import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+"""
 driver.get('https://www.ingrammicrocloud.com/')
 print(driver.title)
 driver.save_screenshot("Pagina-principal.png")
@@ -48,6 +49,36 @@ for lang in langs:
     print lang.get_attribute('href')
 lang=driver.find_element_by_css_selector('a[href*=".com/es"]')
 lang.click()
-time.sleep(3)
+time.sleep(3)"""
+
+#Caso 1 , recogemos todos los href y titulos de los desplegables
+driver.get('https://www.ingrammicrocloud.com/')
+time.sleep(2)
+agree = driver.find_element_by_css_selector('.agree-button.btn.btn-secondary.btn-sm.custom_agree')
+agree.click()
+for i in range(1,5):
+    menu=driver.find_element_by_css_selector('#block-mainnavigation > div > ul > li:nth-child(%s)' %i)
+    menu.click()
+    time.sleep(1)
+    campos=driver.find_elements_by_css_selector('.link-box.link-box-small')
+    for campo in campos:
+        if campo.text != "":
+            print campo.text
+            print campo.get_attribute('href')
+
+driver.get("https://www.ingrammicrocloud.com/es/es/partner-stories/")
+time.sleep(2)
+for i in range(2,5):
+    menu= driver.find_element_by_css_selector('body > div.dialog-off-canvas-main-canvas > div.wrapper.background-light > div > div:nth-child(2) > div > ul > li:nth-child(%s)' %i)
+    menu.click()
+    time.sleep(2)
+    element = driver.find_element_by_css_selector("body > div.dialog-off-canvas-main-canvas > div.wrapper.background-light > div > div:nth-child(2) > div > ul > li:nth-child(%s) > a" %i)
+    print element.text
+    campos = driver.find_elements_by_css_selector('.partner-box')
+    for campo in campos:
+        if campo.text != "":
+            print campo.text
+            print campo.get_attribute('href')
+
 
 driver.quit()
