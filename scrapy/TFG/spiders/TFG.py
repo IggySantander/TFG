d86f34e6857cc000d75cd5d58e292499de1e05c2
Ignacio Oceja <33689933+IggySantander@users.noreply.github.com>
2019-08-24 17:36:16 +0200

    Finishing coding, 1 part missing
    It is allmost done except blogs of march

--- scrapy/TFG/spiders/TFG.py
+++ scrapy/TFG/spiders/TFG.py
@@ -31,20 +31,28 @@ class HomeSpider(scrapy.Spider):
                 assert(splash:wait(1))
                 
                 --Task 02: Getting all elements of menu
-                
-                for var=1,5 do
-                    menu=splash:select('#block-mainnavigation > div > ul > li:nth-of-type('..var ..')')
-                    assert(menu:mouse_click{})
-                    assert(splash:wait(1))
-                    fields = splash:select_all('.link-box.link-box-small')
-                    for key,value in pairs(fields) do
-                                        if (value:text() ~= "") then
-                                            print(value.node:text())
-                                            print(value.node:getAttribute('href'))
-                                        end
+                local menu = {}
+                local menu_text = {}
+              	local submenu = {}
+              	local submenu_text = {}
+                    menu2 = splash:select_all('#header-')
+                    submenu2 = splash:select_all('.link-box.link-box-small')
+                    --for key,value in pairs(fields) do
+                    --                    if (value:text() ~= "") then
+                    --                       print(value.node:text())
+                    --                        print(value.node:getAttribute('href'))
+                    --                    end
+                    --end
+                    for i, elem in ipairs(menu2) do
+                        table.insert(menu,elem:getAttribute('href')) 
+                        table.insert(menu_text,elem:text())
+                    end
+  					for i, elem in ipairs(submenu2) do
+                        table.insert(submenu,elem.node:getAttribute('href'))
+                        table.insert(submenu_text,elem:text())
                     end
                 
-                end
+                
                 
                 -- Task 03: We enter the blog
                 
@@ -52,6 +60,10 @@ class HomeSpider(scrapy.Spider):
                 assert(element:mouse_click{})
                 assert(splash:wait(1))
                 return{
+                menu=menu,
+                menu_text=menu_text,
+    			submenu=submenu,
+    			submenu_text=submenu_text,
                 png=splash:png(),
                 url=splash:url(),
                 }
@@ -79,7 +91,7 @@ class HomeSpider(scrapy.Spider):
                                         print(splash:url())
                                         nextbutton = splash:select('a[rel="next"]')
                                         assert(nextbutton:mouse_click())
-                                        assert(splash:wait(1))
+                                        assert(splash:wait(1.5))
                                         element= splash:select('a[href*="/blogs/newly-relaunched-ingram-micro-cloud-website-and-blog-are-live/"]')
                                 end
                 assert(splash:wait(1))
@@ -103,19 +115,60 @@ class HomeSpider(scrapy.Spider):
                                 element=splash:select('.logo')
                                 assert(element:mouse_click{})
                                 assert(splash:wait(1))
+                                
+                                -- Task 06 : Finding blog entries with date march 2019
+                                
+                                element= splash:select('#block-mainnavigation > div > ul > li:nth-of-type(4)')
+                                assert(element:mouse_click{})
+                                assert(splash:wait(1))
+                                element= splash:select('#header-Blog')
+                                assert(element:mouse_click{})
+                                assert(splash:wait(1))
+                                nextbutton = splash:select('a[rel="next"]')
+                                while (nextbutton ~= nil) do
+                                        print(splash:url())
+                                        nextbutton = splash:select('a[rel="next"]')
+                                        assert(nextbutton:mouse_click())
+                                        assert(splash:wait(1.5))
+                                        nextbutton = splash:select('a[rel="next"]')
+                                end
+                                blogs = {}
+                                blogsmarch= splash:select_all('.blog-box')
+                                
+                                for i, elem in ipairs(blogsmarch) do
+                                    element = splash:select('' ..elem.. ' >  div:nth-child(2) > div:nth-child(3) > span:nth-child(1) > time:nth-child(1)')
+                                            if string.match(element:text(), "March") then
+                                                if string.match(element:text(), "2019") then
+                                                    table.insert(blogs,element:getAttribute('href'))
+                                                end
+                                            end
+                                end
+                                
                                 -- Task 08: Find all countries and selecting Spain
                                 element= splash:select('.selected-country')
                                 assert(element:mouse_click{})
-                                assert(splash:wait(3))
+                                assert(splash:wait(1.5))
+                                local hrefs = {}
+                                local text = {}
                                 elements=splash:select_all('a[href*="www.ingrammicrocloud"]')
-                                for key,value in pairs(elements) do
-                                        if (value:text() ~= "") then
-                                            print(value.node:text())
-                                            print(value.node:getAttribute('href'))
-                                        end
-                    end
+                                --for key,value in pairs(elements) do
+                                --        if (value:text() ~= "") then
+                                --            print(value.node:text())
+                                --            print(value.node:getAttribute('href'))
+                                --        end
+                                --end
+                                for i, elem in ipairs(elements) do
+                                    if (elem:text() ~= "") then
+                                        text[i]= elem.node:text()
+                                        hrefs[i] = elem.node:getAttribute('href')
+                                    end
+                                end
+
                                 splash:set_viewport_full()
                                 return{
+                                hrefs=hrefs,
+                                text=text,
+                                blogs=blogs,
                                 png=splash:png(),
                                 url=splash:url(),
                                 }
@@ -125,8 +178,7 @@ class HomeSpider(scrapy.Spider):
     # Cuarto Script: Lo mismo que antes, solo que esta vez interactuamos con el elemento, y sacamos una foto de la pagina del blog
 
     script3 = """
-                function main(splash)
-
+               function main(splash)
   								splash:set_viewport_size(1920, 1080)
                                 assert(splash:go(splash.args.url))
                                 assert(splash:wait(1))
@@ -138,27 +190,34 @@ class HomeSpider(scrapy.Spider):
                                 element= splash:select('a[href*="/es/es/partner-stories/"]')
                                 assert(element:mouse_click{})
                                 assert(splash:wait(1))
+                                local submenu= {}
+                                local submenu_text = {}
+                                local menu = {}
+                                local menu_text = {}
                                 for var=2,5 do
                                     element= splash:select('body > div.dialog-off-canvas-main-canvas > div.wrapper.background-light > div > div:nth-child(2) > div > ul > li:nth-child('..var ..')')
-                                    assert(element:mouse_click{})
-                                    assert(splash:wait(1))
-                                    element= splash:select('body > div.dialog-off-canvas-main-canvas > div.wrapper.background-light > div > div:nth-child(2) > div > ul > li:nth-child('..var ..')')
                                     if (element ~= nil) then
-                                        texto= element:text()
-                                        print(texto)
+                                        table.insert(menu_text,element.node:text())
+                                        table.insert(menu,element:getAttribute('href'))
                                     end
+                                end
                                     campos = splash:select_all('.partner-box')
                                     for key,value in pairs(campos) do
-                                        elemento = key,value
                                         if (value:text() ~= "") then
-                                        print(value:text())
-                                        print(value:getAttribute('href'))
+                                        --print(value:text())
+                                        --print(value:getAttribute('href'))
+                                        table.insert(submenu_text,value.node:text())
+                                        table.insert(submenu,value:getAttribute('href'))
                                         end
                                     end
                                     
-                                end
+                                
                                 splash:set_viewport_full()
                                 return{
+                                menu=menu,
+                                menu_text=menu_text,
+                                submenu=submenu,
+                                submenu_text=submenu_text,
                                 png=splash:png(),
                                 url=splash:url(),
                                 }
@@ -207,6 +266,21 @@ class HomeSpider(scrapy.Spider):
         # full decoded JSON data is available as response.data:
         body = ast.literal_eval(response.body)
         imgstring = body['png']
+        menu_text= body['menu_text']
+        menu=body['menu']
+        submenu_text=body['submenu_text']
+        submenu=body['submenu']
+
+        print "Menu Principal:"
+        for elem in menu:
+            print menu_text[elem]
+            print menu[elem]
+
+        print "Menu¡s secundarios:"
+        for elem in submenu:
+            print submenu_text[elem]
+            print submenu[elem]
+
         url = body['url']
         print "processing: " + url
         Image = "Image-1.png"
@@ -245,15 +319,21 @@ class HomeSpider(scrapy.Spider):
         body = ast.literal_eval(response.body)
         png_bytes3 = body['png']
         url = body['url']
+        hrefs = body['hrefs']
+        text = body['text']
+
+        print "Countries available:"
+
+        for elem in text:
+            print  text[elem]
+            print  hrefs[elem]
+
         print "processing: " + url
         Image = "Image-3.png"
         fh = open(Image, "wb")
         fh.write(png_bytes3.decode('base64'))
         fh.close()
         print Image + " has been saved"
-        elements = body['elements']
-        print elements
-        """print response.css("a[href*=blog]::attr(href)").getall()"""
         yield SplashRequest(
             url=response.url,
             callback=self.parse4,
@@ -267,6 +347,20 @@ class HomeSpider(scrapy.Spider):
         body = ast.literal_eval(response.body)
         png_bytes3 = body['png']
         url = body['url']
+        menu_text = body['menu_text']
+        menu = body['menu']
+        submenu_text = body['submenu_text']
+        submenu = body['submenu']
+
+        print "Menu Principal:"
+        for elem in menu:
+            print menu_text[elem]
+            print menu[elem]
+
+        print "Menu¡s secundarios:"
+        for elem in submenu:
+            print submenu_text[elem]
+            print submenu[elem]
         print "processing: " + url
         Image = "Image-4.png"
         fh = open(Image, "wb")
